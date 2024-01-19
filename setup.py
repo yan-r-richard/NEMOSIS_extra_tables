89b6d3517b0f847ce978e74377fa65f74c52b705
yan-r-richard <138736961+yan-r-richard@users.noreply.github.com>
2024-01-19 16:11:30 +1100

    Change name

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nemosis",
-    version="3.5.0",
+    name="nemosis_extra_tables",
+    version="3.5.1",
     author="Nicholas Gorman, Abhijith Prakash",
     author_email="n.gorman305@gmail.com",
     description="A tool for accessing AEMO data.",
